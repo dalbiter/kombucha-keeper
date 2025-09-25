06136b2b46497483f06bfebbf6cb75daa748c3d9
danx <rushn24@gmail.com>
2025-09-24 20:56:38 -0400

    set up basic 404 and error handling

--- back_end/app.js
+++ back_end/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const ExpressError = require("./expressError");
 
 const app = express();
 
@@ -8,5 +9,22 @@ app.use(morgan("dev"));
 app.get("/", (req, res) => {
     res.send("The app is working");
 });
+
+// General 404 error handler
+app.use((req, res, next) => {
+    const e = new ExpressError(404, "Oops there's nothing here!")
+    next(e);
+})
+
+// Generic error handler
+app.use((error, req, res, next) => {
+    let status = error.status || 500;
+    let msg = error.msg || "Internal server error";
+
+    return res.status(status).json({
+        error: { status, msg }
+    });
+});
+
 
 module.exports = app;
