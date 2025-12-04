55f03bcc339da73160dc7bd0d6297c810d2a3f87
danx <rushn24@gmail.com>
2025-12-03 23:48:23 -0500

    working on user model and routes

---
+++ back_end/models/user.js
@@ -0,0 +1,39 @@
+const db = require("../db");
+const ExpressError = require("../expressError");
+
+class User {
+    constructor(id, username, first_name, last_name, email, role_choice, password_hash, created_at) {
+        this.id = id;
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.role_choice = role_choice;
+        this.password_hash = password_hash;
+        this.created_at = created_at;
+    }
+    static async getAll(){
+        const results = await db.query(
+            `SELECT id, username, first_name, last_name, email, role_choice, password_hash, created_at 
+            FROM users`
+            );
+        console.log(results)
+        const users = results.rows.map(r => new User(
+            r.id, r.username, r.first_name, r.last_name, r.email, r.role_choice, r.password_hash, r.created_at
+        ))
+        console.log(users);
+        return users;
+    };
+    static async findById(id){
+        const results = await db.query(
+            `SELECT id, username, first_name, last_name, email, role_choice, password_hash, created_at 
+            FROM users
+            WHERE id = $1`,
+            [id]    
+            )
+            const u = results.rows[0];
+            return new User(u.id, u.username, u.first_name, u.last_name, u.email, u.role_choice, u.password_hash, u.created_at)
+    }
+};
+
+module.exports = User;
--- back_end/routes/userRoutes.js
+++ back_end/routes/userRoutes.js
@@ -1,15 +1,25 @@
 const express = require("express");
-const ExpressError = require("../expressError");
 const router = new express.Router();
-const db = require("../db");
+const User = require("../models/user");
+const { request } = require("../app");
 
 router.get("/", async (req, res, next) => {
     try {
-        const results = await db.query("SELECT * FROM users")
-        return res.json({ users: results.rows });
+        let users = await User.getAll();
+        return res.json({users});
     } catch (e) {
         return next (e);
     }
 });
+
+router.get("/:id", async (req, res, next) => {
+    try{
+        let {id} = req.params
+        let user = await User.findById(id)
+        return res.json(user)
+    } catch(e) {
+        return next(e);
+    }
+})
 
 module.exports = router;
